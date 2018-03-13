95fc84646b4517874c5f892d0c99c274eda48eb7
fridrihgreen <self-killer@list.ru>
2018-03-13 11:09:31 +0600

    lab2 v1.0

---
+++ lab2/l2/FindingMaxLen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l2
+{
+    public class FindingMaxLen
+    {
+        public string MaxLength(string input)
+        {
+            string[] str = input.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int maxlen = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxlen)
+                {
+                    maxlen = str[i].Length;
+                    index = i;
+                }
+            }
+            return str[index];
+        }
+    }
+}
---
+++ lab2/l2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Дана строка-предложение на русском языке.
+//Вывести самое длинное слово в предложении (если таких слов несколько, то вывести первое из них). 
+
+using System;
+
+namespace l2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+         {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            
+            FindingMaxLen word = new FindingMaxLen();
+
+            string str = word.MaxLength(input);
+
+                Console.Write("Самое длинное слово: {0}", str);
+                Console.WriteLine();
+                Console.ReadLine();
+         }
+    }
+}
