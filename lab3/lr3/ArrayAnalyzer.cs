4a864cfd84392bd4ae492e641e355fb7980f126f
FG <self-killer@list.ru>
2018-03-27 10:13:34 +0600

    lab3 v1.0

---
+++ lab3/lr3/ArrayAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿namespace lr3
+{
+    class ArrayAnalyzer
+    {
+        public int Comparator(int A, int B, ref int[] array)
+        {
+            int index = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > A && B > array[i])
+                {
+                    index = i;
+                }
+
+                else continue;
+            }
+            return index;
+        }
+
+    }
+}
---
+++ lab3/lr3/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Дан целочисленный массив A размера 11. Вывести номер первого из
+тех его элементов A[i], которые удовлетворяют двойному
+неравенству: A[1] < A[i] < A[10]. Если таких элементов нет, то
+вывести 0.*/
+
+using System;
+
+namespace lr3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] array = new int[11];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 20);
+            }
+
+            int A = array[1];
+            int B = array[10];
+
+            ArrayAnalyzer indexFinder = new ArrayAnalyzer();
+            int index = indexFinder.Comparator(A, B, ref array);
+
+            Console.Write("Массив из случайных чисел от 0 до 20: \n");
+            foreach (int i in array)
+                Console.Write(i + " ");
+
+            Console.WriteLine("\n\nРезультат: ");
+
+            if (index == -1)
+                Console.WriteLine("\n00");
+            else
+                Console.WriteLine("\n" + array[index] + "[" + index + "]");
+
+            Console.ReadLine();
+        }
+    }
+}
