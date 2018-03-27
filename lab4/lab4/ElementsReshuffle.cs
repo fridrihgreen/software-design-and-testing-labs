e5c68c6c7493d13ed3d61fb6ac556f68acdce850
fridrihgreen <self-killer@list.ru>
2018-03-27 11:01:41 +0600

    lan4 v1.0

---
+++ lab4/lab4/ElementsReshuffle.cs
@@ -0,0 +1,14 @@
+﻿namespace lab4
+{
+    class ElementsReshuffle
+    {
+        public int[] Reshuffler(int[] array)
+        {
+            int temp = array[2];
+            array[2] = array[4];
+            array[4] = temp;
+
+            return  array;
+        }
+    }
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,32 @@
+﻿//Поменять местами элементы на 3 и 5 позициях 
+
+using System;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] array = new int[5];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 20);
+            }
+
+            Console.Write("Исходный массив: \n");
+            foreach (int i in array)
+                Console.Write(i + " ");
+
+            ElementsReshuffle reshuffler = new ElementsReshuffle();
+            int[] newArray = reshuffler.Reshuffler(array);
+
+            Console.Write("\n\nМассив с перестановкой: \n");
+            foreach (int i in newArray)
+                Console.Write(i + " ");
+            Console.Read();
+        }
+    }
+}
