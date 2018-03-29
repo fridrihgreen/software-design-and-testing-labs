3d22afc5d9184d50e0ade45baa09acaca2d4272b
fridrihgreen <self-killer@list.ru>
2018-03-29 09:48:15 +0600

    lab5 v1.0

---
+++ lab5/lab5/ArrayInsert.cs
@@ -0,0 +1,41 @@
+﻿namespace lab5
+{
+    class ArrayInsert
+    {
+        public int NewLengthDeterm(int[] array, int size)
+        {
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0 && array[i] % 2 != 0)
+                {
+                    j++;
+                }
+            }
+
+            return j;
+        }
+
+        public int[] MaxInserter(int j, int[] array, int size, int max)
+        {
+        int[] newArray = new int[size + j];
+
+            for (int i = 0, k = 0; i < size; i++)
+            {
+                if (array[i] < 0 && array[i] % 2 != 0)
+                {
+                    newArray[k] = array[i];
+                    newArray[k + 1] = max;
+                    k++;
+                }
+                else
+                {
+                    newArray[k] = array[i];
+                }
+                k++;
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,58 @@
+﻿//Вставить максимальное положительное после каждого нечетного
+//отрицательного.
+
+using System;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int max = -100;
+            int size = 0;
+            while (size <= 0)
+            {
+                Console.WriteLine("Введите количество элементов:");
+                size = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (size <= 0) Console.WriteLine("Ошибка ввода!");
+            }
+
+            Random rnd = new Random();
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-10, 10);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+           
+            ArrayInsert inserter = new ArrayInsert();
+            int newIndex = inserter.NewLengthDeterm(array, size);
+            int[] newArray = inserter.MaxInserter(newIndex, array, size, max);
+
+
+            Console.Write("Исходный массив: \n");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.Write("\n\nНовый массив: \n");
+            foreach (int i in newArray)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
