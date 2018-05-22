e92e3f4469371f570caef0fa8bb951e97b346a5b
fridrihgreen <self-killer@list.ru>
2018-05-22 10:18:37 +0600

    lab6
    lab6 v1.0

---
+++ lab6/ConsoleApp5/DeleteFromArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab6
+{
+    public class DeleteFromArray
+    {
+
+        public List<int> DeleteElement(List<int> array)
+        {
+            int first = Math.Abs(array[0]);
+
+            for (int i = 1; i < array.Count; i++)
+            {
+
+                if (array[i] % first == 0)
+                {
+                    array.RemoveAt(i);
+                }
+            }
+
+            return array;
+        }
+
+
+    }
+}
---
+++ lab6/ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿//Удалить элементы массива кратные его первому элементу(первый элемент при этом не рассматривать)
+
+using System;
+using System.Collections.Generic;
+
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>();
+            Console.WriteLine("Введите количество элементов: ");
+            int amountOfElements = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < amountOfElements; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива: ");
+                int element = int.Parse(Console.ReadLine());
+                array.Add(element);
+            }
+
+            DeleteFromArray deleteFromArray = new DeleteFromArray();
+            array = deleteFromArray.DeleteElement(array);
+
+            Console.WriteLine("Результат: ");
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
